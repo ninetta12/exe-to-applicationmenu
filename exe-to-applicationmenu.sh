4a43a4f886a3e56c42769d99d5fb436a0415dcf6
ninetta12 <Ninapreppy38@gmail.com>
2026-04-03 09:55:36 +0200

    Update exe-to-applicationmenu.sh
    Added support for .sh applications/scripts to be added to the application menu

--- exe-to-applicationmenu.sh
+++ exe-to-applicationmenu.sh
@@ -4,20 +4,43 @@
 EXE=$1
 PATH=$(/bin/dirname $EXE)
 EXENAME=$(/bin/basename $EXE)
-CATEGORIES=$(/bin/zenity --entry --title="In wich application menu category should i be?
+
+#quiz
+CATEGORIES=$(/bin/zenity --entry --title="In wich application menu category should I be?
 
 (type 'Game' if you want it to be in the 'Games' menu)" )
-LOCATION=$2
 
-/bin/wrestool -x -t 14 $EXE > ~/Pictures/icons/$EXENAME.ico
-ICON=~/Pictures/icons/$EXENAME.ico
+FILETYPE=$(/bin/zenity --entry --title="What type of executeable am I?
 
-#make the .desktop file in the applications menu
+(.sh or .exe)")
+
+#get the .exe icon if its a .exe file, or else it asks for the path to an icon
+echo $FILETYPE
+if [ $FILETYPE == ".exe" ]
+then
+    #extract the icon from the .exe
+    /bin/wrestool -x -t 14 $EXE > ~/Pictures/icons/$EXENAME.ico
+    ICON=~/Pictures/icons/$EXENAME.ico
+
+    DESKTOPPROGRAM="Exec= wine $EXE"
+else
+    #ask for an icon
+    ICON=$(/bin/zenity --entry --title="Add the path to the icon you want
+
+    (for example: ~/Pictures/icons/your_icon.png)")
+
+
+    DESKTOPPROGRAM="Exec= sh $EXENAME"
+    echo $DESKTOPPROGRAM
+fi
+
+
+#make the .desktop file
 (echo "[Desktop Entry]"
 echo "Categories=$CATEGORIES"
 echo "Comment[en_US]="
 echo "Comment="
-echo "Exec=env LUTRIS_SKIP_INIT=1 wine $EXE"
+echo "$DESKTOPPROGRAM"
 echo "GenericName[en_US]="
 echo "GenericName="
 echo "Icon=$ICON"
@@ -32,4 +55,5 @@ echo "TryExec=lutris"
 echo "Type=Application"
 echo "X-KDE-SubstituteUID=false"
 echo "X-KDE-Username=") > ~/.local/share/applications/$EXENAME.desktop
+
 
